8e695e12413d1e9793ed2fb3711e8c3225b9bc6c
DIO1010 <rio.anime_otaku@outlook.com>
2020-07-08 23:07:47 +0900

    コミット対象の修正
    本来コミットの対象にするべきものが対象になっていなかったことを修正
    また，コミットの対象もコミットさせる

---
+++ NovelGame/Src/Layers/Layer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Layers
+{
+  public class Layer
+  {
+    public virtual Bitmap Image { get; set; }
+    public virtual Point Point { get; set; }
+    public virtual string Target { get; set; }
+    public virtual string MessageText { get; set; }
+    public virtual Size Size { get; set; }
+
+
+
+    public virtual void DrawLayer(Graphics g) { }
+    public virtual void SetTextIndexMax() { }
+    public virtual bool IsTextCountMax() { return false; }
+
+    public string GetNeedInformation(string text)
+    {
+      Match match = Regex.Match(text, @"^(\w*?)=(?<param>\w*?)$");
+      return match.Groups["param"].ToString();
+    }
+  }
+}
---
+++ NovelGame/Src/States/State.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace States
+{
+  public class State
+  {
+    public virtual bool Control { get; set; }
+
+    public virtual void Update(StateManage SM, Form GF) { }
+    public virtual Bitmap GetDrawImage() { return null; }
+    public virtual void ReloadScript(string filename) { }
+
+  }
+}
